f62db1b3e352f8f75f7559b04bb2ec7cdf959ab6
Sadra <sadra.z.kh@gmail.com>
2019-04-05 23:24:08 +0430

    1

---
+++ ViewsInDetail/ViewsInDetail/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ViewsInDetail.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            string[] fruits = { "apple", "orange", "pear" };
+            return View(fruits);
+        }
+
+        public ActionResult MyAction()
+        {
+            string[] fruits = { "apple", "orange", "pear" };
+            return View(fruits);
+        }
+    }
+}
